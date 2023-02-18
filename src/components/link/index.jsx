cc03b217344f98bd7042dfca23b7218f9d6c27ec
Khaetbek <hayotbek-web@mail.ru>
2023-02-18 10:19:14 +0500

    #fix #image #navigation #link #social Фикс и разраб компонент

--- src/components/link/index.jsx
+++ src/components/link/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
 export default function Url(props) {
-    const { href, text, classes } = props
+    const { href, text,children, classes } = props
     return (
-        <a href={href} className={classes}>{text}</a>
+        <a href={href} className={classes}>{text || children}</a>
     )
+}
+
+Url.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.element),
+        PropTypes.element.isRequired
+    ])
 }
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -6,6 +6,14 @@ import Logo from "./../../images/logo.png"
 import "./navigation.scss"
 import Url from "./../link"
 import Phone from "./../../images/icons/phone-icon.svg"
+import Social from '../social'
+import VK from "./../../images/icons/vk-icon.svg"
+import Instagram from "./../../images/icons/instagram-icon.svg"
+import TikTok from "./../../images/icons/tik-tok-icon.svg"
+import Basket from "./../../images/icons/basket-icon.svg"
+import Menu from "./../../images/icons/menu-icon.svg"
+
+
 
 export default function Navigation() {
     return (
@@ -22,7 +30,26 @@ export default function Navigation() {
                             <Url text={"+7861 21791 18"} href={"#!"} classes="Navigation__link" />
                         </div>
                     </div>
-                    <div className="Navigation__right"></div>
+                    <div className="Navigation__right">
+                        <div className="Navigation__social">
+                            <Social href={"#!"} classes={"Navigation__social-item"} src={VK} />
+                            <Social href={"#!"} classes={"Navigation__social-item"} src={Instagram} />
+                            <Social href={"#!"} classes={"Navigation__social-item"} src={TikTok} />
+                        </div>
+                        <div className="Navigation__lang">
+                            <button className="Navigation__lang-item">en</button>
+                            <button className="Navigation__lang-item">ru</button>
+                        </div>
+                        <div className="Navigation__option">
+                            <button className="Navigation__option-item">
+                                <Image src={Basket} alt={"Basket icon"} />
+                            </button>
+                            <button className="Navigation__option-item">
+                                <Image src={Menu} alt={"Basket icon"} />
+                            </button>
+                        </div>
+
+                    </div>
                 </div>
             </Container>
         </nav>
