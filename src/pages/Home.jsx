002c71309ce999796c288eca69894a0b5d6f2079
Khaetbek <hayotbek-web@mail.ru>
2023-02-27 19:39:47 +0500

    #fix #image #jsx #scss Разработал header основного страниц

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import Container from '../components/container'
-import Heading from '../components/heading'
-import Background1 from "./../images/header/background-1.jpeg"
+import Header from '../components/header'
 
 export default function Home() {
   return (
-    <header className='section-header' style={{ backgroundImage: `url(${Background1})`, }} >
-      <div className="header-wrapper">
-        <Container>
-          <div className="header__content">
-            <Heading heading={"h1"} >
-              AgroScout - беспилотные технологии для сельского хозяйства и промышлености
-            </Heading>
-          </div>
-        </Container>
-      </div>
-    </header>
+    <Header />
   )
 }
