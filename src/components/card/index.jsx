8b85d1fd5c4ac7ec58fdc9842ab350278131cf6a
Khaetbek <hayotbek-web@mail.ru>
2023-03-03 23:37:47 +0500

    #fix #home Разработал продукт

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useCart } from 'react-use-cart'
 import "./cards.scss"
 
 export default function Card(props) {
-    const { card, img, title, body } = props
+    const { card, img, title, body, brand, model, price, obj } = props
+    const { addItem } = useCart()
 
     switch (card) {
         case "card":
@@ -10,7 +12,7 @@ export default function Card(props) {
                 <div className='card card__default'>
                     <div className="card__default-top">
                         <div className="card__default-icon">
-                            <img src={img} alt={"Image error upload"} />
+                            <img src={img} alt="Image error upload" />
                         </div>
                         <h3 className="card__default-title">{title}</h3>
                     </div>
@@ -19,7 +21,31 @@ export default function Card(props) {
                     </div>
                 </div>
             )
-            break;
+            
+        case "product":
+            return (
+                <div className="card card__product">
+                    <div className="card__product-imageholder">
+                        <img src={img} alt="Error upload" />
+                    </div>
+                    <div className="card__product-desc">
+                        <h3 className="card__product-title">
+                            {title}
+                        </h3>
+                        <ul className='card__product-list'>
+                            <li className='card__product-item'>Бренд: {brand}</li>
+                            <li className='card__product-item'>Модель: {model}</li>
+                        </ul>
+                        <p className='card__product-price'>${price}</p>
+
+                        <div className="card__product-buttons">
+                            <button className='card__product-button' onClick={() => addItem(obj)}>Добавить в корзину</button>
+                        </div>
+                    </div>
+
+                </div>
+            )
+            
         default:
             console.log("error")
             break;
