969a273091233e8b9a74bc993a9b99723b85efe6
Khaetbek <hayotbek-web@mail.ru>
2023-03-14 20:47:36 +0500

    #fix #card #home-news Разработал карт. новостей

--- src/components/link/index.jsx
+++ src/components/link/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import PropTypes from "prop-types"
+import "./link.scss"
 
 export default function Url(props) {
     const { href, text,children, classes } = props
     return (
         <a href={href} className={classes}>{text || children}</a>
     )
-}
-
-Url.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.element),
-        PropTypes.element.isRequired
-    ])
 }
